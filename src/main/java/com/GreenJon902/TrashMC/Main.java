e036edc3c957fc3aa15e360aa5559a7247ed8f0b
jj b <jj@thebabers.at>
2021-01-07 15:47:46 +0000

    got basic app in place

---
+++ src/main/java/com/GreenJon902/TrashMC/Main.java
@@ -0,0 +1,11 @@
+package com.GreenJon902.TrashMC;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static Logger LOGGER = Logger.getLogger("TrashMC_Logger");
+
+    static void start() {
+        LOGGER.info("Starting TrashMC");
+    }
+}
---
+++ src/test/java/com/GreenJon902/TrashMC/TestMain.java
@@ -0,0 +1,10 @@
+package com.GreenJon902.TrashMC;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+    @Test
+    public void testLucky() {
+        Main.start();
+    }
+}
