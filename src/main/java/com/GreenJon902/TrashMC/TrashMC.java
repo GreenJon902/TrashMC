a57cd27191943400a3f0cc1cee06fa96d66df84e
GreenJon902 <GreenJon902@gmail.com>
2021-07-11 20:48:29 +0100

    maven project

--- src/main/java/com/GreenJon902/TrashMC/TrashMC.java
+++ src/main/java/com/GreenJon902/TrashMC/TrashMC.java
@@ -6,13 +6,13 @@ import java.util.logging.Logger;
 public class TrashMC {
     private static final Logger LOGGER = Logger.getLogger(TrashMC.class.getName());
 
-    static void setup() {
+    public static void setup() {
         System.setProperty("java.util.logging.SimpleFormatter.format",
                 "[%1$tF %1$tT] [%4$-7s] %5$s %n");
     }
 
 
-    public static void main(String[] args) {
+    public static void start() {
         LOGGER.info("TrashMC Setting up");
 
         setup();
