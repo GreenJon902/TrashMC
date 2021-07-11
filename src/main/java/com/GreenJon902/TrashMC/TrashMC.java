55357ac9377cf7a2dd67c3f99c84169966316dde
GreenJon902 <GreenJon902@gmail.com>
2021-07-11 19:57:11 +0100

    suff

--- src/main/java/com/GreenJon902/TrashMC/TrashMC.java
+++ src/main/java/com/GreenJon902/TrashMC/TrashMC.java
@@ -4,14 +4,17 @@ package com.GreenJon902.TrashMC;
 import java.util.logging.Logger;
 
 public class TrashMC {
-    private static Logger LOGGER = Logger.getLogger(TrashMC.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(TrashMC.class.getName());
 
     static void setup() {
         System.setProperty("java.util.logging.SimpleFormatter.format",
                 "[%1$tF %1$tT] [%4$-7s] %5$s %n");
     }
 
-    static void start() {
+
+    public static void main(String[] args) {
+        LOGGER.info("TrashMC Setting up");
+
         setup();
 
         LOGGER.info("TrashMC Starting");
