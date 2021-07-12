1d3f1c79b2b8294622a6f8caf3d9547af7066cd5
GreenJon902 <GreenJon902@gmail.com>
2021-07-12 17:26:24 +0100

    rename

--- src/main/java/com/GreenJon902/TrashMC/blocks/Block.java
+++ src/main/java/com/GreenJon902/TrashMC/blocks/Block.java
@@ -1,7 +1,7 @@
 package com.GreenJon902.TrashMC.blocks;
 
 public class Block {
-    public static String id = "None";
+    public static String identifier = "None";
     int[] pos = {0, 0, 0};
 
 }
