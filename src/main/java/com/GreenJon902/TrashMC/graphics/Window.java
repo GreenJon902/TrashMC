ca093ff95d028eb02516413054520ba4260faeb9
GreenJon902 <GreenJon902@gmail.com>
2021-07-16 19:00:12 +0100

    fix

--- src/main/java/com/GreenJon902/TrashMC/graphics/Window.java
+++ src/main/java/com/GreenJon902/TrashMC/graphics/Window.java
@@ -1,23 +1,34 @@
 package com.GreenJon902.TrashMC.graphics;
 
 
+import com.GreenJon902.TrashMC.Logger.Logger;
+import com.GreenJon902.TrashMC.TrashMC;
+
 import javax.swing.*;
 
 public class Window extends JFrame {
+    private final static Logger LOGGER = new Logger(Window.class.getName());
+
+
     int fps = 1;
 
     public Window() {
         super("TrashMC");
+
+        LOGGER.logDebug("Initializing Window");
         setSize(1080, 720);
 
         build();
+        LOGGER.logInfo("Initialized Window");
     }
 
     private void build() {
         this.add(BlockRenderer.canvas);
+        LOGGER.logDebug("Built");
     }
 
     public void open() {
+        LOGGER.logDebug("Opening Window");
         setVisible(true);
         this.mainLoop();
     }
